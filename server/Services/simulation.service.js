ba934e6d46e850cf7f4304e71d624cdfc262562d
Shaswat Mishra <mishra3shaswat@gmail.com>
2026-04-01 12:32:11 +0530

    feat: complete simulation engine with metrics, lifecycle tracking and duration

--- server/Services/simulation.service.js
+++ server/Services/simulation.service.js
@@ -1,11 +1,48 @@
 import Simulation from "../models/simulation.model.js"
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve,ms))
 
+const simulateRequest = async (url, failureRate, latency) => {
+  await sleep(latency)
+  const shouldFail = Math.random() < failureRate / 100
+  if (shouldFail)
+     throw new Error("Simulated failure")
 
-const processSimulation = async (config) => {
+  return { success: true, responseTime: latency }
+}
+
+const processSimulation = async (config) => 
+{
     const data = await Simulation.create(config)
-    return data
+   let updatedmid = await Simulation.findByIdAndUpdate(
+       data._id,
+       {status: "running"},{ new: true }
+    )
+    const start = Date.now()
+    // console.log(updatedmid)
+    const promise = Array.from({length:config.concurrentUsers},() => simulateRequest(config.apiUrl,config.failureRate,config.latency))
+    const result = await Promise.allSettled(promise)
+    const end = Date.now()
+    let successCount = 0
+    let failureCount = 0
+    const totalDuration = end - start
+    result.forEach(res =>
+         {
+        if(res.status=='fulfilled')
+            successCount++
+        else 
+            failureCount++
 
-} 
+    })
 
+    const avgResTime = config.latency
 
+   const updated =  await Simulation.findByIdAndUpdate(
+       data._id,
+       {successCount,failureCount,averageResponseTime:avgResTime,status: "completed",totalDuration},{ new: true }
+    )
+
+    console.log(`Updated Successfully ${updated._id}` )
+    return updated
+
+}
 export default processSimulation
